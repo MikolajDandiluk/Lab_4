e936bc3f54ec9adc469e6f2e06aea5d535fa1f16
MikolajDaniluk <mikolaj.daniluk@op.pl>
2023-07-03 23:42:27 +0200

    skrypt tworzący logi

--- skrypt.sh
+++ skrypt.sh
@@ -3,6 +3,12 @@
 if [[ $1 == "--date" ]]; then
     current_date=$(date "+%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
+elif [[ $1 == "--logs" ]]; then
+    for i in {1..100}; do
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
+    done
 else
-    echo "Niepoprawne użycie skryptu. Aby wyświetlić dzisiejszą datę, wpisz: skrypt.sh --date"
+    echo "Bledny argument"
 fi
