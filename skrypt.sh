13d4aac9286373eb45e52f9b844f99babb325657
MikolajDaniluk <mikolaj.daniluk@op.pl>
2023-07-03 23:52:14 +0200

    Revert "dodanie do skryptu tworzenia ilości logów"
    This reverts commit de4b8eef24d702f129bbb2c7e2df38e41d49d237.

--- skrypt.sh
+++ skrypt.sh
@@ -4,16 +4,10 @@ if [[ $1 == "--date" ]]; then
     current_date=$(date "+%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
 elif [[ $1 == "--logs" ]]; then
-    if [ -n "$2" ]; then
-        zmienna=$2
-    else
-        zmienna=100
-    fi
-    for ((i=1; i<=$2; i++)) 
-    do
+    for i in {1..100}; do
         echo "Nazwa pliku: log$i.txt" > log$i.txt
         echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
-        echo "Data: $(date "+%Y-%m-%d")" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
     done
 else
     echo "Bledny argument"
